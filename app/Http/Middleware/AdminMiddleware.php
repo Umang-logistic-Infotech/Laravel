d0b64147b7b71210b735d8055304fd41d0360fe0
Umang Jadav <umang@logisticinfotech.co.in>
2025-10-24 12:43:44 +0530

    Added role bas middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+use function Laravel\Prompts\alert;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            return redirect()->route('login')->with('error', 'You must be an admin to access this page.');
+        } elseif ($user->userType === 'student') {
+            return redirect()->back()->with('error', 'You cannot edit student details.');
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,13 @@ use App\Http\Controllers\InvokableController;
 use App\Http\Controllers\ResourceController;
 use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/', function () {
     $students = (new StudentController)->index(request());
     $user = Auth::user();
 
     return view('homeOld', compact('students', 'user'));
-
     // return view('welcome');
 })->middleware('auth');
 
@@ -43,7 +43,7 @@ Route::controller(StudentController::class)->group(function () {
     Route::get('getStudentsCount',  'getStudentsCount')->middleware('auth');
     Route::get('addStudent', 'addStudent')->middleware('auth');
     Route::post('createStudent', 'createStudent')->middleware('auth');
-    Route::get('getStudent/{id}', 'getStudent')->middleware('auth');
+    Route::get('getStudent/{id}', 'getStudent')->middleware(AdminMiddleware::class);
     Route::post('updateStudent/{id}', 'updateStudent')->middleware('auth');
     Route::delete('deleteStudent/{id}', 'deleteStudent')->middleware('auth');
     Route::get('deletedStudents', 'deletedStudents')->middleware('auth');
